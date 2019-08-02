d343754487a0cd3a4a476b451da3f1bd026ce968
Grégory Nam <37612938+Gregory-Nam@users.noreply.github.com>
2019-08-02 11:00:29 +0200

    modification du main

--- main.cpp
+++ main.cpp
@@ -5,88 +5,22 @@
 #include <vector>
 
 
-//int main()
-//{
-//	// création de la fenêtre
-//	sf::RenderWindow window(sf::VideoMode(700, 600), "My window");
-//	float tailleCellule = 20;
-//	float dimensionMatrice = 10;
-//	Grille grille(tailleCellule, dimensionMatrice);
-//	window.draw(grille)
-//	//création de la grille :
-//	grille.init();
-//	//std::vector <std::vector <sf::RectangleShape>> celulles;
-//	// on fait tourner le programme tant que la fenêtre n'a pas été fermée
-//	while (window.isOpen())
-//	{
-//		// on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
-//		sf::Event event;
-//		while (window.pollEvent(event))
-//		{
-//			// fermeture de la fenêtre lorsque l'utilisateur le souhaite
-//			if (event.type == sf::Event::Closed)
-//				window.close();
-//			//if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-//			//	float x = sf::Mouse::getPosition(window).x / 20;
-//			//	float y = sf::Mouse::getPosition(window).y / 20;
-//			//	celulles[x][y].setFillColor(sf::Color::Blue);
-//			//}
-//				
-//		}
-//
-//		// effacement de la fenêtre en noir
-//		window.clear(sf::Color::Black);
-//
-//		float tailleCellule = 20;
-//		float nombreCelluleCarre = 10;
-//		
-//		/*bool creation = false;
-//		for (unsigned i = 0; i < nombreCelluleCarre; ++i) {
-//			if (celulles.size() == 0)
-//				creation = true;
-//
-//			if(creation) {
-//				celulles.resize(nombreCelluleCarre);
-//				for (unsigned j = 0; j < nombreCelluleCarre; ++j) {
-//					sf::RectangleShape rec(sf::Vector2f(tailleCellule, tailleCellule));
-//					rec.setPosition(i * tailleCellule, j * tailleCellule);
-//					rec.setOutlineColor(sf::Color(0, 0, 0));
-//					rec.setOutlineThickness(1);
-//					celulles[i].push_back(rec);
-//					window.draw(rec);
-//				}
-//			}
-//			else {
-//				for (unsigned j = 0; j < nombreCelluleCarre; ++j) {
-//					window.draw(celulles[i][j]);
-//				}
-//			}
-//			
-//		}*/
-//
-//
-//
-//		// fin de la frame courante, affichage de tout ce qu'on a dessiné
-//		window.display();
-//	}
-//}
-
 int main()
 {
 	unsigned dimension = 100, taille = 15;
 	Grille g(taille, dimension);
-	// création de la fenêtre
+	// crÃ©ation de la fenÃªtre
 	sf::RenderWindow window(sf::VideoMode((dimension * taille) + 300, dimension * taille), "Jeu de la vie");
 	
 	g.init();
-	// on fait tourner le programme tant que la fenêtre n'a pas été fermée
+	// on fait tourner le programme tant que la fenÃªtre n'a pas Ã©tÃ© fermÃ©e
 	while (window.isOpen())
 	{
-		// on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
+		// on traite tous les Ã©vÃ¨nements de la fenÃªtre qui ont Ã©tÃ© gÃ©nÃ©rÃ©s depuis la derniÃ¨re itÃ©ration de la boucle
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// fermeture de la fenêtre lorsque l'utilisateur le souhaite
+			// fermeture de la fenÃªtre lorsque l'utilisateur le souhaite
 			if (event.type == sf::Event::Closed)
 				window.close();
 			if (event.type == sf::Event::MouseButtonPressed && sf::Mouse::Left == event.mouseButton.button) {
@@ -104,7 +38,7 @@ int main()
 					// c'est ici qu'on dessine tout
 					// window.draw(...);
 					window.draw(g);
-					// fin de la frame courante, affichage de tout ce qu'on a dessiné
+					// fin de la frame courante, affichage de tout ce qu'on a dessinÃ©
 					window.display();
 					if (event.type == sf::Event::MouseButtonPressed && sf::Mouse::Right == event.mouseButton.button) break;
 
@@ -112,12 +46,12 @@ int main()
 			}
 		}
 
-		// effacement de la fenêtre en noir
+		// effacement de la fenÃªtre en noir
 		window.clear(sf::Color::Black);
 		// c'est ici qu'on dessine tout
 		// window.draw(...);
 		window.draw(g);
-		// fin de la frame courante, affichage de tout ce qu'on a dessiné
+		// fin de la frame courante, affichage de tout ce qu'on a dessinÃ©
 		window.display();
 	}
 
